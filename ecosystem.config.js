9eafdb21f7ee25b069dd6f37a213c1417aaa9dfa
zhangyan36 <zhangyan36@58ganji.com>
2020-12-17 17:26:51 +0800

    update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,11 +1,18 @@
 module.exports = {
-    apps: [
-      {
-        name: 'NuxtAppName',
-        exec_mode: 'cluster',
-        instances: 'max', // Or a number of instances
-        script: './node_modules/nuxt/bin/nuxt.js',
-        args: 'start',
+  apps: [
+    {
+      name: "NuxtAppName",
+      exec_mode: "cluster",
+      instances: "max", // Or a number of instances
+      script: "./node_modules/nuxt/bin/nuxt.js",
+      args: "start",
+      env: {
+        NODE_ENV: "development"
       },
-    ],
-  }
+      env_production: {
+        NODE_ENV: "production"
+      },
+      log_date_format: "YYYY-MM-DD HH:mm Z",
+    }
+  ]
+};
